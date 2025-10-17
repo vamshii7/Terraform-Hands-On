6d627aa91e360bf16350012afc1d7c76513822a1
Vamshi Krishna <48650579+vamshii7@users.noreply.github.com>
2025-10-17 16:43:35 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -9,10 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = "fce97da6-3054-4b20-9501-3b4eb524e886"
-  tenant_id       = "70d14e82-b071-41e7-a84b-d17297930785"
-  subscription_id = "5db43b7e-7ec8-40ed-9358-9f8a6adc0baf"
-  client_secret   = "VGr8Q~aQyMKt6PkRD1xNLcYSWnVch~nMNeZKfaEl"
 }
 
 resource "azurerm_resource_group" "rg" {
