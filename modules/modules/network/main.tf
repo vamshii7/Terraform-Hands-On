0b58f8806b8bd57ae7b326442aab7ff7379c0eec
Vamshi Krishna <48650579+vamshii7@users.noreply.github.com>
2025-10-17 16:45:08 +0530

    Add output for subnet ID in main.tf

--- modules/modules/network/main.tf
+++ modules/modules/network/main.tf
@@ -13,3 +13,6 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.subnet_address_prefixes
 }
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
+}
