c301dc8e8cfa15344db2453fbfd9c12bbb9b6284
vamshii7 <vamshi9849@live.com>
2025-10-26 17:41:38 +0530

    updated main.tf

--- Terraform+CI-CD/for-each/main.tf
+++ Terraform+CI-CD/for-each/main.tf
@@ -1,57 +1,57 @@
-locals {
-  resource_groups = var.resource_groups
-  network_details = var.network_details
-  vm_details      = var.vm_details
-}
+# locals {
+#   resource_groups = var.resource_groups
+#   network_details = var.network_details
+#   vm_details      = var.vm_details
+# }
 
-# -------------------------
-# Debug outputs
-# -------------------------
+# # -------------------------
+# # Debug outputs
+# # -------------------------
 
-# Raw strings (exactly as passed from pipeline)
-output "debug_resource_groups_raw" {
-  value = var.resource_groups
-  sensitive = true   # mark sensitive to avoid leaking in logs
-}
+# # Raw strings (exactly as passed from pipeline)
+# output "debug_resource_groups_raw" {
+#   value = var.resource_groups
+#   sensitive = true   # mark sensitive to avoid leaking in logs
+# }
 
-output "debug_network_details_raw" {
-  value = var.network_details
-  sensitive = true
-}
+# output "debug_network_details_raw" {
+#   value = var.network_details
+#   sensitive = true
+# }
 
-output "debug_vm_details_raw" {
-  value = var.vm_details
-  sensitive = true
-}
+# output "debug_vm_details_raw" {
+#   value = var.vm_details
+#   sensitive = true
+# }
 
-# Decoded maps (usable in for_each)
-output "debug_resource_groups_decoded" {
-  value = local.resource_groups
-}
+# # Decoded maps (usable in for_each)
+# output "debug_resource_groups_decoded" {
+#   value = local.resource_groups
+# }
 
-output "debug_network_details_decoded" {
-  value = local.network_details
-}
+# output "debug_network_details_decoded" {
+#   value = local.network_details
+# }
 
-output "debug_vm_details_decoded" {
-  value = {
-    for k, v in local.vm_details :
-    k => {
-      vm_name = v.vm_name
-      # mask the password so it doesn’t print in logs
-      vm_password = "*****"
-    }
-  }
-}
+# output "debug_vm_details_decoded" {
+#   value = {
+#     for k, v in local.vm_details :
+#     k => {
+#       vm_name = v.vm_name
+#       # mask the password so it doesn’t print in logs
+#       vm_password = "*****"
+#     }
+#   }
+# }
 
 resource "azurerm_resource_group" "this" {
-  for_each = local.resource_groups
+  for_each = var.resource_groups
   name     = each.value.rg_name
   location = each.value.rg_location
 }
 
 resource "azurerm_virtual_network" "this" {
-  for_each            = local.network_details
+  for_each            = var.network_details
   name                = each.value.vnet_name
   address_space       = [each.value.vnet_cidr]
   location            = azurerm_resource_group.this[each.key].location
@@ -59,7 +59,7 @@ resource "azurerm_virtual_network" "this" {
 }
 
 resource "azurerm_subnet" "this" {
-  for_each             = local.network_details
+  for_each             = var.network_details
   name                 = each.value.subnet_name
   resource_group_name  = azurerm_resource_group.this[each.key].name
   virtual_network_name = azurerm_virtual_network.this[each.key].name
@@ -67,7 +67,7 @@ resource "azurerm_subnet" "this" {
 }
 
 resource "azurerm_network_interface" "this" {
-  for_each            = local.vm_details
+  for_each            = var.vm_details
   name                = "${each.value.vm_name}-nic"
   location            = azurerm_resource_group.this[each.key].location
   resource_group_name = azurerm_resource_group.this[each.key].name
@@ -80,7 +80,7 @@ resource "azurerm_network_interface" "this" {
 }
 
 resource "azurerm_linux_virtual_machine" "this" {
-  for_each              = local.vm_details
+  for_each              = var.vm_details
   name                  = each.value.vm_name
   location              = azurerm_resource_group.this[each.key].location
   resource_group_name   = azurerm_resource_group.this[each.key].name
